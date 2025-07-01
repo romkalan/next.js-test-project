814855893f096d8c028a9e06d341f073e39c80f5
romkalan <romkalan@mail.ru>
2025-07-01 23:25:55 +0300

    work with page.js and layout.js

---
+++ src/app/restaurants/layout.js
@@ -0,0 +1,12 @@
+function RestaurantsLayout({children}) {
+    return (
+        <html lang="en">
+        <body>
+        Restaurants Layout
+        {children}
+        </body>
+        </html>
+    );
+}
+
+export default RestaurantsLayout;
---
+++ src/app/restaurants/page.js
@@ -0,0 +1,9 @@
+function RestaurantsPage() {
+    return (
+        <div>
+            <h1>Restaurants Page</h1>
+        </div>
+    );
+}
+
+export default RestaurantsPage;
