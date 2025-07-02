bb14dde50bc19761ed714cbc2f92f13729e4ca9e
romkalan <romkalan@mail.ru>
2025-07-02 20:55:03 +0300

    work with layout.js files and grouping dir pages

---
+++ src/app/(authorized)/restaurants/menu/layout.js
@@ -0,0 +1,10 @@
+function MenuLayout({children}) {
+    return (
+        <div>
+            Menu Layout
+            {children}
+        </div>
+    );
+}
+
+export default MenuLayout;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>...</h1>
+      <h1>This is home Page</h1>
     </div>
   );
 }
